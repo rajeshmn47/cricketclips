104a66fd36b16c4cf1f08b8a2e6320e04540598d
rajeshmn47 <rajeshmn47@gmail.com>
2025-10-16 14:21:09 +0530

    added new backend url

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -69,7 +69,7 @@ function geturl() {
         // return "https://dreamelevenclonebackend-g4iw.onrender.com";
         // return "https://fantasycricket4u.com";
         // return "https://backend.dreambet11.io";
-        return "https://fango11.duckdns.org";
+        return "https://gclteam.duckdns.org";
         // return "https://mangogames.fun/api";
     }
     // return "https://dreamelevenclonebackend-7l34.onrender.com";
